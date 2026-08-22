b2dea2f98fb0b6c941af5e556a26e09211d82e75
Zeeshan Waris <zeeshanwaris3345@gmail.com>
2026-08-22 17:04:29 +0500

    Added additional functions such as get_creds which return the credentials tto gmail API

---
+++ additional_functions.py
@@ -0,0 +1,39 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+import os
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.send"
+]
+
+def get_creds():
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file(
+           'token.json',
+           SCOPES 
+        )
+
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh_token(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json',
+                SCOPES
+            )
+
+            creds = flow.run_local_server(port = 0)
+
+
+    with open("token.json", "w") as token:
+        token.write(creds.to_json())
+
+    return creds
